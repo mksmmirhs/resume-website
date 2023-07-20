e4f7610efb7261995d7545080e3f9e952263cf30
Merajul Islam <meraj.exe@gmail.com>
2023-07-20 07:15:00 +0600

    contact section

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import About from './components/About';
 import Services from './components/Services';
 import Contributors from './components/Contributors';
 import Portfolio from './components/Portfolio';
-import Contact from './components/Contact ';
+import Contact from './components/Contact';
 
 const App = () => {
   return (
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,32 @@
 import React from 'react';
 
+// import data ]
+import { contact } from '../data';
+//import logo
+import Logo from '../assets/img/logo.svg';
+// import components
+import Social from './Social';
+
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <section
+      className="section before:content-ornament
+    before:flex  before:justify-center lg:before:mb-[44px]"
+      id="contact"
+    >
+      <div className="container mx-auto">
+        <h2 className="section-title text-accent">{contact.title}</h2>
+        <p className="section-subtitle">{contact.subtitle}</p>
+        {/* logo */}
+        <div className="max-w-[200px] lg:max-w-xs mx-auto mb-28">
+          <img className="w-full" src={Logo} alt="" />
+        </div>
+        <div className="flex justify-center">
+          <Social />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
